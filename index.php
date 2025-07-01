49021946418aaf5d48bfa32ec6ce8f8223f2229e
markosjr <markosjr1969@gmail.com>
2025-06-30 22:32:45 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Site Imobiliária</title>
+
+    <link rel="stylesheet" href="_cdn/css/bootstrap_person.css">
+    <link rel="stylesheet" href="_cdn/css/app.css">
+</head>
+<body>
+
+<button class="btn btn-front icon-info">teste</button>
+
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
