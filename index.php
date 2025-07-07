dc96fb84bd16b7fc4fe885d7827f3b7e647d096c
markosjr <markosjr1969@gmail.com>
2025-07-07 20:20:23 -0300

    header bar

--- index.php
+++ index.php
@@ -10,7 +10,27 @@
 </head>
 <body>
 
-<button class="btn btn-front icon-info">teste</button>
+<header class="main_header">
+
+    <div class="header_bar bg-front">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                    <i class="icon-location-arrow"></i>
+                    <p class="my-auto ml-3">Avenida Guilherme de Paula Xavier, 1723 Campo Mourão/Pr</p>
+                </div>
+                <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                    <i class="icon-clock-o"></i>
+                    <p class="my-auto ml-3">Seg/Sex: 09:00h - 18:00 </br> Sáb: 09:00 - 13:00 </p>
+                </div>
+                <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                    <i class="icon-envelope"></i>
+                    <p class="my-auto ml-3">contato@imobsantamaria.com.br </br>+55 (44) 9999-1234</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
 
 <script src="node_modules/jquery/dist/jquery.min.js"></script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
