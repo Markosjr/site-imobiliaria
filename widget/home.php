f87f7af6da39ef57c5ecdef7802ed5c6cebf22ae
markosjr <markosjr1969@gmail.com>
2025-07-09 22:51:20 -0300

    finalizando segunda dobra

--- widget/home.php
+++ widget/home.php
@@ -193,7 +193,7 @@
                 <div class="card main_properties_item">
                     <div class="img-responsive-16y9">
                         <a href="">
-                            <img src="properties/1/capa.jpg" class="card-img-top" alt="">
+                            <img src="properties/2/capa.jpg" class="card-img-top" alt="">
                         </a>
                     </div>
                     <div class="card-body">
@@ -224,7 +224,7 @@
                 <div class="card main_properties_item">
                     <div class="img-responsive-16y9">
                         <a href="">
-                            <img src="properties/1/capa.jpg" class="card-img-top" alt="">
+                            <img src="properties/3/capa.jpg" class="card-img-top" alt="">
                         </a>
                     </div>
                     <div class="card-body">
@@ -232,6 +232,113 @@
                         <p class="main_properties_item_category">Imóvel Residencial</p>
                         <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
                         <p class="main_properties_item_price text-front">R$ 400.000,00 </p>
+                        <a href="" class="btn btn-front w-100"> Ver imóvel </a>
+                    </div>
+                    <div class="card-footer text-muted d-flex">
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="">
+                            <p class="text-muted">1</p>
+                        </div>
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/garage.png" class="img-fluid" alt="">
+                            <p class="text-muted">4</p>
+                        </div>
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid" alt="">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+
+        </div>
+    </div>
+</section>
+
+<section class="main_properties py-5">
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center border-bottom border-front mb-5">
+            <h1 class="text-front"> <b> Para Alugar </b> </h1>
+            <a href="" class="text-front"> Ver mais</a>
+        </div>
+
+        <div class="row">
+
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16y9">
+                        <a href="">
+                            <img src="properties/4/capa.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <h2><a href="" class="text-front"> Linda Casa no Campeche com vista para o Mar </a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 3.000,00 </p>
+                        <a href="" class="btn btn-front w-100"> Ver imóvel </a>
+                    </div>
+                    <div class="card-footer text-muted d-flex">
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="">
+                            <p class="text-muted">1</p>
+                        </div>
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/garage.png" class="img-fluid" alt="">
+                            <p class="text-muted">4</p>
+                        </div>
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid" alt="">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16y9">
+                        <a href="">
+                            <img src="properties/5/capa.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <h2><a href="" class="text-front"> Linda Casa no Campeche com vista para o Mar </a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 2.600,00 </p>
+                        <a href="" class="btn btn-front w-100"> Ver imóvel </a>
+                    </div>
+                    <div class="card-footer text-muted d-flex">
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="">
+                            <p class="text-muted">1</p>
+                        </div>
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/garage.png" class="img-fluid" alt="">
+                            <p class="text-muted">4</p>
+                        </div>
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid" alt="">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16y9">
+                        <a href="">
+                            <img src="properties/5/8771b309-8314-4023-89a8-1f1b01165075.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <h2><a href="" class="text-front"> Linda Casa no Campeche com vista para o Mar </a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 3.500,00 </p>
                         <a href="" class="btn btn-front w-100"> Ver imóvel </a>
                     </div>
                     <div class="card-footer text-muted d-flex">
