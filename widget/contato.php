0019b40e8d23301c777ae2075cf6ce58366a3127
markosjr <markosjr1969@gmail.com>
2025-07-11 00:18:53 -0300

    pagina de contato

--- widget/contato.php
+++ widget/contato.php
@@ -1,1 +1,58 @@
-CONTATO
+<div class="main_contact py-5 bg-light text-center">
+    <div class="container">
+            <h1 class="text-front"> Entre em Contato Conosco</h1>
+            <p class="mb-0"> Quer conversar e ter um atendimento diferenciado em busca do seu imóvel dos sonhos?</p>
+            <p>Preencha o formulário abaixo!</p>
+
+            <div class="row text-start">
+                <form action="">
+                    <h2 class="icon-envelope text-black-50"> Envie um e-mail </h2>
+                    <div class="form-group mb-4">
+                        <input type="text" class="form-control" placeholder="Insira seu nome">
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <input type="email" class="form-control" placeholder="Insira seu melhor e-mail">
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <input type="tel" class="form-control" placeholder="Insira seu telefone com DDD">
+                    </div>
+
+                    <div class="form-group">
+                        <textarea type="teste" rows="5" placeholder="Escreva sua mensagem" class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group text-end mt-4">
+                        <button class="btn btn-front">Enviar Contato</button>
+                    </div>
+                </form>
+            </div>
+    </div>
+</div>
+
+<div class="main_contact_types py-5">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-4">
+                <h2 class="icon-envelope"> Por E-mail </h2>
+                <p> Iremos entrar em contato com você assim que possível.</p>
+                <p class="pt-4"><a href="" class="text-front"> Email@email.com.br </a></p>
+            </div>
+
+            <div class="col-4">
+                <h2 class="icon-phone"> Por Telefone </h2>
+                <p> Estamos disponiveis no número abaixo no horário comercial.</p>
+                <p class="text-front pt-2">+55 (44) 9999-1234</p>
+            </div>
+
+            <div class="col-4">
+                <h2 class="icon-share-alt"> Redes Sociais </h2>
+                <p> Fique por dentro de tudo o que compartilhamos nas Redes Sociais!</p>
+                <p> <button class="btn btn-front icon-whatsapp icon-notext"> </button> <button class="btn btn-front icon-facebook icon-notext"> </button> <button class="btn btn-front icon-instagram icon-notext"> </button></p>
+
+
+            </div>
+        </div>
+    </div>
+</div>
